45a352bcf815117bffb2cef16571bd6de8475dc6
gy5271992-tech <gy5271992@gmail.com>
2026-04-04 02:54:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // =====================
 const db = mysql.createConnection({
     host:     process.env.DB_HOST,
+     port:    process.env.DB_PORT,
     user:     process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
